18eb1b8304ca5cd443020288b922ebbb9ce2300d
Ahmadou Bamba Diagne <diagne.bambahmadou@gmail.com>
2022-03-04 19:57:24 +0000

    Update app

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # python-utils
 A repo who contains Python tools
+This tool create folders and move currents files in each type of extension<br> 
+<br>You can run the script without dependancies but
+
+### if you want to make your own exe file <br>
+
+Create virtual env `py -m venv venv` <br>
+Download and install the dependancies `pip -r install requirements.txt` <br>
+Run the pyinstaller to create exe file  `pyinstaller --onefile -c "rangement.py"` <br>
+Go the dist directory and you will see the exe file !
--- rangement.py
+++ rangement.py
@@ -1,10 +1,16 @@
 import os
+print("========================BIENVENUE DANS LE CLASSEUR DE FICHIERS=================================")
+
 files = [file for file in os.listdir() if(not os.path.isdir(file))]
-file_to_jump=os.path.basename(__file__)
-files.remove(file_to_jump)
-files.remove(os.path.splitext(file_to_jump)[0]+'.exe')
-print(files)
+file_to_jump = os.path.basename(__file__)
+exe_escape_file, exe_extension = os.path.splitext(file_to_jump)
+
+if(os.path.isfile(exe_escape_file+'.exe')):
+    files.remove(exe_escape_file+'.exe')
+if(os.path.isfile(file_to_jump)):
+    files.remove(file_to_jump)
 files_extension = []
+
 for file in files:
     filename, file_extension = os.path.splitext(file)
     if(file_extension):
@@ -20,6 +26,11 @@ for file in files:
 
     filename, file_extension = os.path.splitext(file)
     if(file_extension):
+        print("Deplacement du fichier {0} vers Fichiers {1} ".format(
+            file, file_extension[1:]))
         os.replace(file, "Fichiers {0}/{1} ".format(file_extension[1:], file))
     else:
+        print("Deplacement du fichier {0} vers Fichiers {1} ".format(
+            file, file_extension[1:]))
         os.replace(file, "Fichiers sans extension/{0} ".format(file))
+quit__ = input("Appuyer sur Entrée pour quitter")
